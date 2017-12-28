b1a2b1e7d8c967d767dbb1eae713c6160077ae80
Carl Zimmerman <carlpzimmerman@gmail.com>
2017-12-27 20:08:15 -0500

    adds rest client and snipped from btc online

--- app/models/btcprice.rb
+++ app/models/btcprice.rb
@@ -1,2 +1,13 @@
 class Btcprice < ActiveRecord::Base
+
+  require 'rest-client'
+  require 'json'
+
+  base_url = "http://blockchain.info/ticker"
+  response = RestClient.get base_url
+  data = JSON.load response
+  cool = data["USD"]
+  @test = JSON.pretty_generate cool
+
+
 end
