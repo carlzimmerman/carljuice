336342a9375d04a2c1bf54f8f44a741da32bf8a6
Carl Zimmerman <carlpzimmerman@gmail.com>
2017-12-27 20:16:02 -0500

    got the rest client json to work

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,17 @@
 class HomeController < ApplicationController
  require 'json'
  require 'open-uri'
+ require 'rest-client'
+ require 'json'
 
 
 def index
+
+  base_url = "https://blockchain.info/ticker"
+  response = RestClient.get base_url
+  data = JSON.load response
+  cool = data["USD"].first
+  @test = JSON.pretty_generate cool
 end
 
 def ticker
--- app/models/btcprice.rb
+++ app/models/btcprice.rb
@@ -3,7 +3,7 @@ class Btcprice < ActiveRecord::Base
   require 'rest-client'
   require 'json'
 
-  base_url = "http://blockchain.info/ticker"
+  base_url = "https://blockchain.info/ticker"
   response = RestClient.get base_url
   data = JSON.load response
   cool = data["USD"]
